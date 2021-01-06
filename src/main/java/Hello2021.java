c26d3184358c217cd5fe9edc0291ccfecc807ebf
jinseon0328 <wishofsun@daum.net>
2021-01-06 20:43:07 +0900

    .

---
+++ src/main/java/Hello2021.java
@@ -0,0 +1,7 @@
+package a;
+class Hello2021{
+public static void main(String[] args){
+System.out.println("Hello,2021!");
+
+    }
+}
---
+++ src/main/java/b/Hello0106.java
@@ -0,0 +1,6 @@
+package b;
+class Hello0106{
+    public static void main(String[] args){
+        System.out.println("Hello,0106!");
+    }
+}
